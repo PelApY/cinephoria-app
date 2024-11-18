7ddea66d21a4f057c107923baad84bdd3eeeda5a
PelApY <anthony.pelong@gmail.com>
2024-11-18 17:24:22 +0100

    Résolution affichage cinemas dans le footer

--- templates/home.php
+++ templates/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cinéphoria - Landing Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-H6qaep/OVpHQC2Xa2yaoaE4UNXczKncoUp9b2OxFYr2M5swe1KxJl50MjRHpbZVb" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 </head>
 <body>
